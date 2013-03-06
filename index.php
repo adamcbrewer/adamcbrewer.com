6ab6a63aaa3549ce9a183cd299840b5d41177b0a
Adam Brewer <adamcbrewer@gmail.com>
2013-03-06 15:31:35 +0000

    Init

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1,requiresActiveX=true">
+	<link rel="openid2.provider" href="https://www.google.com/accounts/o8/ud?source=profiles">
+	<link rel="openid2.local_id" href="http://www.google.com/profiles/adamcbrewer">
+
+
+	<title>Adam Brewer : Web Developer</title>
+	<meta name="robots" content="all">
+
+	<link type="text/plain" rel="author" href="humans.txt" />
+	<meta name="author" content="Adam Brewer">
+
+	<meta name="description" content="Front-end developer and Webmaker">
+	<meta name="keywords" content="webmaker, web developer, front-end developer, brewer, brewer logic, brewerlogic">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<? // FACEBOOK OPEN GRAPH ?>
+	<meta property="og:title" content="Adam Brewer"/>
+	<meta property="og:image" content="assets/img/me.jpg"/>
+	<meta property="og:site_name" content="Adam Brewer"/>
+	<meta property="og:description" content="Front-end developer and Webmaker"/>
+
+	<link href="assets/css/core.css" rel="stylesheet">
+
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-19769555-1']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
+</head>
+<body>
+	<div class="wrapper">
+
+		<header class="header">
+			<p>
+				<?= date('F Y', time('now')); ?><br />
+				<small>Developer at Saatchi, London</small>
+			</p>
+		</header>
+
+		<h1>Hi, I'm Adam Brewer.</h1>
+		<p>I'm a feverent front-end web developer and Webmaker; I love what I do and <a href="http://saatchi.co.uk/" target="_blank" title="Saatchi &amp; Saatchi">who</a> I do it for.</p>
+		<p>My passions lie in running ultra-marathons, coding for the Web, wine and paleo food. I used to be an Aircraft Technician in the British Army so I have quite a few stories to tell.</p>
+		<p>You can find my work at <a href="http://brewerlogic.com" target="_blank">brewerlogic.com</a> and lots of other little places around the web.</p>
+
+		<footer class="footer">
+
+			<a href="http://twitter.com/adamcbrewer" target="_blank" title="Twitter">twitter</a> <a href="http://github.com/adamcbrewer" target="_blank" title="Github">github</a> <a href="https://plus.google.com/u/0/109651434155076896860/" target="_blank" title="Google Plus">google</a> <a href="http://za.linkedin.com/in/adamcbrewer" target="_blank" title="LinkedIn">linkedin</a>
+
+		</footer>
+
+	</div>
+</body>
+</html>
